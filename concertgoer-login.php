fdd61152a80cb317b03b621b7b83a10dcb5795c9
kaialami <kaialami@students.cs.ubc.ca>
2023-11-17 22:32:01 -0800

    temporary login page to test passing userID using POST

---
+++ concertgoer-login.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Concertgoer Login</title>
+    </head>
+
+    <body>
+        <h1>Placeholder</h1>
+
+
+        <form method="post" action="concertgoer.php">
+            <input type="hidden" name="userID" value="kahnsert123">
+            <input type="submit" value="Login as kahnsert123">
+        </form>
+
+    </body>
+
+</html>
