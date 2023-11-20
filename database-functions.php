f3efa5b40289dd1d16fdd1a7e4ce8d9463f62ba5
kaialami <kaialami@students.cs.ubc.ca>
2023-11-20 00:51:52 -0800

    added bind sql function

--- database-functions.php
+++ database-functions.php
@@ -31,22 +31,58 @@
         $statement = OCIParse($db_conn, $cmdstr);
 
         if (!$statement) {
-            echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+            echo "<br><p>Cannot parse the following command: " . $cmdstr . "</p><br>";
             $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
-            echo htmlentities($e['message']);
+            echo "<p>" . htmlentities($e['message'])  . "</p><br>";
             $success = False;
         }
 
         $r = OCIExecute($statement, OCI_DEFAULT);
         if (!$r) {
-            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            echo "<br><p>Cannot execute the following command: " . $cmdstr . "</p><br>";
             $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-            echo htmlentities($e['message']);
+            echo "<p>" . htmlentities($e['message']) . "</p><br>";
             $success = False;
         }
 
         return $statement;
     }
+
+    function executeBoundSQL($cmdstr, $list) {
+        /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
+    In this case you don't need to create the statement several times. Bound variables cause a statement to only be
+    parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
+    See the sample code below for how this function is used */
+
+        global $db_conn, $success;
+        $statement = OCIParse($db_conn, $cmdstr);
+
+        if (!$statement) {
+            echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($db_conn);
+            echo htmlentities($e['message']);
+            $success = False;
+        }
+
+        foreach ($list as $tuple) {
+            foreach ($tuple as $bind => $val) {
+                //echo $val;
+                //echo "<br>".$bind."<br>";
+                OCIBindByName($statement, $bind, $val);
+                unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+            }
+
+            $r = OCIExecute($statement, OCI_DEFAULT);
+            if (!$r) {
+                echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+                $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
+                echo htmlentities($e['message']);
+                echo "<br>";
+                $success = False;
+            }
+        }
+    }
+
 
     // returns array where indexes 0, 1, 2 are date, time, AM/PM, respectively
     function formatDateTime($datetime) {
